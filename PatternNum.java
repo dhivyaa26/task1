60014b8ead8aac4d4ed5b5f7ca8daae337c30af4
dhivyaa26 <dhivyaa.26@gmail.com>
2024-05-30 17:35:58 +0530

    Add files via upload

--- PatternNum.java
+++ PatternNum.java
@@ -3,14 +3,13 @@ package task1;
 public class PatternNum {
     public static void main(String[] args) {
         for (int i = 5; i > 0; i--) {
-            for (int j = 5; j > 0; j--) {
-                if (i == 5) {
-                    System.out.print(i);
-                    System.out.print(" ");
-                } else {
-                    System.out.print(j);
-                    System.out.print(" ");
-                }
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            for(int k = 1; k < i; k++ ) {
+                System.out.print(i);
+                System.out.print(" ");
             }
             System.out.println();
         }
